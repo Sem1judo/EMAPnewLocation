765e815036e55d8f65658b56465c10db35bc1e3e
Andrii_Shylin <anderwerty@gmail.com>
2020-01-15 23:51:39 +0200

    lesson 2

---
+++ src/main/java/com/bank/repository/impl/AccountRepositoryImpl.java
@@ -0,0 +1,34 @@
+package com.bank.repository.impl;
+
+import com.bank.domain.Account;
+import com.bank.repository.AccountRepository;
+
+import java.util.List;
+
+public class AccountRepositoryImpl implements AccountRepository {
+    //
+    @Override
+    public void save(Account entity) {
+
+    }
+
+    @Override
+    public Account findById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public List<Account> findAll() {
+        return null;
+    }
+
+    @Override
+    public void update(Account entity) {
+
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+
+    }
+}
---
+++ src/main/java/com/bank/repository/impl/UserRepositoryImpl.java
@@ -0,0 +1,42 @@
+package com.bank.repository.impl;
+
+import com.bank.domain.User;
+import com.bank.repository.UserRepository;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class UserRepositoryImpl implements UserRepository {
+    private final Map<Integer, User> userIdToUser = new HashMap<>();
+
+    @Override
+    public User findByEmail(String email) {
+        return null;
+    }
+
+    @Override
+    public void save(User entity) {
+
+    }
+
+    @Override
+    public User findById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public List<User> findAll() {
+        return null;
+    }
+
+    @Override
+    public void update(User entity) {
+
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+
+    }
+}
